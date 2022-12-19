bdd0159186cacf99b2a3cd4dd275f6ca571042e4
Naureen <naureen.imran123@yahoo.com>
2022-12-19 08:20:09 +0000

    practice basic button

--- AnytimeMarketplace/components/TextInputPractice.js
+++ AnytimeMarketplace/components/TextInputPractice.js
@@ -1,34 +1,45 @@
 import React, { useState } from "react";
-import { Text, View, StyleSheet, SafeAreaView, TextInput } from "react-native";
+import { Text, View, StyleSheet, SafeAreaView, TextInput, Button } from "react-native";
 function TextInputPractice() {
 
-    const [text, onChangeText] = React.useState("Useless Text");
-    const [number, onChangeNumber] = React.useState(null);
+    const [name, setName] = useState('');
+    const [sumbitted, setSubmitted] = useState(false);
+    const onPressHandler = () => {
+        setSubmitted(!sumbitted);
+    };
 
     return (
-        <SafeAreaView>
+        <SafeAreaView style={styles.body}>
+            <Text>Please enter your name:</Text>
             <TextInput
                 style={styles.input}
-                onChangeText={onChangeText}
-                value={text}
+                onChangeText={(value) => setName(value)}
+                placeholder='e.g Tom'
             />
-            <TextInput
-                style={styles.input}
-                onChangeText={onChangeNumber}
-                value={number}
-                placeholder="useless placeholder"
-                keyboardType="numeric"
+            <Button
+                title={sumbitted ? 'Clear' : "Submit"}
+                onPress={onPressHandler}
+                disabled={sumbitted}
             />
+            {sumbitted ?
+                <Text>Your name is {name}</Text> : null}
         </SafeAreaView>
     );
 };
 
 const styles = StyleSheet.create({
+    body: {
+        alignItems: 'center'
+    },
     input: {
         height: 40,
+        width: 200,
         margin: 12,
         borderWidth: 1,
         padding: 10,
+        borderRadius: 5,
+        textAlign: 'center',
+        fontSize: 20
     },
 });
 
